ac1ac37d9c2afd3e98f67eb93398e79141ae2d50
Administrator <Administrator@WIN-TSJTEJB6C1T>
2024-09-18 12:33:48 +0800

    fix-115-upload---2

--- drivers/115/types.go
+++ drivers/115/types.go
@@ -1,7 +1,7 @@
 package _115
 
 import (
-	"github.com/power721/115driver/pkg/driver"
+	"github.com/SheltonZhu/115driver/pkg/driver"
 	"github.com/alist-org/alist/v3/internal/model"
 	"github.com/alist-org/alist/v3/pkg/utils"
 	"time"
